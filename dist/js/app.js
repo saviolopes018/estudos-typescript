2e35bd53f8bb409ff2a83233ae38229542245a74
savio.lopes <savio.lopes@cidadania.gov.br>
2022-04-26 22:44:33 -0300

    Modelling and rules

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,9 @@
-console.log('Importação feita');
+import { Negociacao } from "./models/negociacao.js";
+
+const negociacao = new Negociacao(new Date(), 10, 100);
+
+console.log(negociacao);
+
+negociacao.quantidade = 10000;
+
+console.log(negociacao);
